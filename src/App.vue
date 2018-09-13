e5442dccdfa3535ec6ed0f15b6e6b1acfeb33776
feng chen <1624689089@qq.com>
2018-09-13 13:35:28 +0800

    配置vue-meta和element-ui

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,28 @@
       <a href="/help">Help</a> |
       <a href="/contactus">contactus</a>
     </div>
+    <el-button>默认按钮</el-button>
     <router-view/>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'App',
+
+  metaInfo: {
+    title: '看tv-Kantv-全球华人首家在线视频分享网站',
+    // titleTemplate: '%s | My Awesome Webapp',
+    meta: [
+      { vmid: 'keywords', name: 'keywords', content: '看tv-Kantv-全球华人首家在线视频分享网站'},
+      { vmid: 'description', name: 'description', content: '看TV（KanTV）是全球华人首家影视视频分享平台，网站包含最新的电视剧、美剧、日韩剧、华语电影、好莱坞电影、以及各种动漫和重大体育赛事直播。在这里，有你想看的一切！'},
+    ],
+    // <meta name="Keywords" content="看tv-Kantv-全球华人首家在线视频分享网站" />
+    // <meta name="Description" content="看TV（KanTV）是全球华人首家影视视频分享平台，网站包含最新的电视剧、美剧、日韩剧、华语电影、好莱坞电影、以及各种动漫和重大体育赛事直播。在这里，有你想看的一切！"/>
+  }
+}
+</script>
+
 
 <style lang="scss">
 @import "@/assets/scss/global.scss";
@@ -21,8 +40,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  background: $color-primary;
-  width: px(200);
+  background: $color-blue;
 }
 #nav {
   padding: 30px;
---
+++ src/plugins/element.js
@@ -0,0 +1,6 @@
+// 默认全部加载，如需按需加载请看文档
+import Vue from 'vue'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(Element)
